1a655f83fef06bd977c84f16da41125c4565f7bb
aknorw <15245704+aknorw@users.noreply.github.com>
2020-07-20 15:46:34 +0200

    Add Figma access token condition

--- scripts/importIconsFromFigma.js
+++ scripts/importIconsFromFigma.js
@@ -2,6 +2,8 @@ const { writeFile } = require("fs").promises;
 const path = require("path");
 
 async function main() {
+  if (!process.env.FIGMA_ACCESS_TOKEN) throw new Error("No Figma access token");
+
   const fileName = `icon-${new Date().getTime()}`;
   const dummyIcon = "<svg></svg>";
   await writeFile(
